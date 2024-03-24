2eafc9a06a06d9141243190a1080a9051a05d6e3
zerosicx <hcsofteng@gmail.com>
2024-03-24 16:10:15 +1300

    Initialies eloquent project

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-red-500 font-bold"> Hello Eloquent</p>
+      <Button variant="zerosicx" size="sm">
+        Some button here
+      </Button>
+    </>
+  );
+}
