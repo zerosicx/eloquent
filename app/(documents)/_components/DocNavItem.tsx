a9df78ea6be1c4a89df4ed7e58cd181b0a0800ad
zerosicx <hcsofteng@gmail.com>
2024-05-28 22:15:12 +1200

    Implemented recursive API calls for new documents

--- app/(documents)/_components/DocNavItem.tsx
+++ app/(documents)/_components/DocNavItem.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface DocNavItemProps {
   id?: Id<"documents">;
@@ -30,7 +34,39 @@ const DocNavItem = ({
   onExpand,
   icon: Icon,
 }: DocNavItemProps) => {
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event?.stopPropagation();
+    onExpand?.();
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+  const handleCreateDocument = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = create({
+      title: "Untitled",
+      parentDocument: id,
+    }).then((documentId) => {
+      if (!expanded) {
+        onExpand?.();
+      }
+      // TODO: implement routes to document pages
+      // router.push(`documents/${documentId}`);
+    });
+
+    toast.promise(promise, {
+      loading: "Creating child document...",
+      success: "Successfully created child document!",
+      error: "Problem creating a child document 😓",
+    });
+  };
 
   return (
     <div
@@ -45,8 +81,8 @@ const DocNavItem = ({
       {!!id && (
         <div
           role="button"
-          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-          onClick={() => {}}
+          className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
+          onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shirnk-0 text-muted-foreground/50" />
         </div>
@@ -61,6 +97,17 @@ const DocNavItem = ({
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-sm">⌘</span>K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={handleCreateDocument}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
